97e0f79920e1e7e7568b18807587f811871e4e3b
Patter1989 <125509804+Patter1989@users.noreply.github.com>
2024-08-03 23:06:43 +0200

    contactForm added

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,33 @@
-// import { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 // import Section from "./components/Section/Section.jsx";
 import ContactForm from "./components/ContactForm/ContactForm.jsx"
 // import Notification from "./components/Notification/Notification.jsx";
 import ContactList from "./components/ContactList/ContactList.jsx";
 import SearchBox from "./components/SearchBox/SearchBox.jsx";
 import contacts from "./Contacts.json"
+import { nanoid } from "nanoid";
 
 
 
 const App = () => {
-	
-	
+	const [users, setUsers ] = useState(contacts)
+	const onAddContact = (contact) => {
+		const finalContact = {
+			...contact,
+			id: nanoid(),
+		}
+
+		setUsers([finalContact, ...contacts,]);
+		console.log(finalContact, contact);
+	}
 	return (
 		<div>
 			<h1>Phonebook</h1>
-				<ContactForm />
+			<ContactForm
+				onAddContact={onAddContact} />
 				<SearchBox />
 				<ContactList
-					contacts={contacts} />
+					contacts={users} />
 		</div>
 	);
 };
--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,11 +1,40 @@
-// import css from "./ContactForm.module.css"
+import css from "./ContactForm.module.css"
 
 
-const ContactForm = () => {
+const ContactForm = ({onAddContact}) => {
+	const handleSubmit = (event) => {
+		event.preventDefault()
+		const formElements = event.currentTarget.elements;
+		const name = formElements.contactName.value;
+		const number = formElements.contactNumber.value;
+		const contactObj = {
+			name,
+			number,
+		}
+		onAddContact(contactObj);
+		event.currentTarget.reset();
+		console.log(name, number);
+	}
   return (
-		<div>
-			
-		</div>
+		<form onSubmit={handleSubmit} className={css.form}>
+			<p>Name</p>
+			<label className={css.label}>
+				<input
+					type='text'
+					name='contactName'
+					required
+				/>
+			</label>
+			<p>Number</p>
+			<label className={css.label}>
+				<input
+					type='tel'
+					name='contactNumber'
+					required
+				/>
+			</label>
+			<button type="submit" className={css.addContactBtn}>✌️Add contact✌️</button>
+		</form>
 	);
 }
 
